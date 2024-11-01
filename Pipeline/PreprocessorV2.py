a8ec0ff92a214fb4bebb50787e452cbaff868ddc
Ivan <ivan.franken89@gmail.com>
2024-11-01 22:58:07 +0100

    Updated Pipeline Feature Calulation

--- Pipeline/PreprocessorV2.py
+++ Pipeline/PreprocessorV2.py
@@ -23,41 +23,32 @@ class HandExtractionTransformer(BaseEstimator, TransformerMixin):
         return np.array(processed_images)
 
     def _extract_hand(self, image):
-        # Check if the image is loaded successfully
         if image is None:
             return
 
-        # Apply sharpening filter
         sharpen_kernel = np.array([[-1, -1, -1],
                                    [-1, 9, -1],
                                    [-1, -1, -1]], dtype=np.float32)
         sharpened = cv.filter2D(image, -1, sharpen_kernel)
 
-        # Apply Gaussian Blur
         blurred = cv.GaussianBlur(sharpened, (11, 11), 0)
 
-        # Detect edges
         edges = cv.Canny(blurred, 50, 125)
 
-        # Dilate edges
         dilated_edges = cv.dilate(edges, cv.getStructuringElement(cv.MORPH_RECT, (3, 3)), iterations=2)
 
-        # Find contours
-        contours, _ = cv.findContours(dilated_edges, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        contours = cv.findContours(dilated_edges, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
         largest_contour = max(contours, key=cv.contourArea)
 
-        # Draw largest contour
         contour_image = np.zeros_like(image, dtype=np.uint8)
         cv.drawContours(contour_image, [largest_contour], -1, 255, cv.FILLED)
 
-        # Determine minY, maxY, minX, maxX with 50-pixel offset logic
         image_height = image.shape[0]
         min_y = min(point[0][1] for point in largest_contour)
         max_y = max(point[0][1] for point in largest_contour)
         distance_to_bottom = image_height - max_y
         horizontal_edge_y = 0 if min_y < distance_to_bottom else image_height
 
-        # Initialize minX and maxX based on 50-pixel offset
         min_x = float('inf')
         max_x = float('-inf')
 
@@ -69,7 +60,6 @@ class HandExtractionTransformer(BaseEstimator, TransformerMixin):
 
         center_of_contours_width = min_x + ((max_x - min_x) // 2)
 
-        # Find closest points to left and right edges
         closest_point_left_edge = (-1, np.inf if horizontal_edge_y == 0 else -np.inf)
         closest_point_right_edge = (-1, np.inf if horizontal_edge_y == 0 else -np.inf)
 
@@ -84,14 +74,11 @@ class HandExtractionTransformer(BaseEstimator, TransformerMixin):
                         horizontal_edge_y == image_height and y > closest_point_right_edge[1]):
                     closest_point_right_edge = (x, y)
 
-        # Draw line between closest points
         cv.line(contour_image, closest_point_left_edge, closest_point_right_edge, 255, 5)
 
-        # Find contours again on the updated contour image
         contours, _ = cv.findContours(contour_image, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
         largest_contour = max(contours, key=cv.contourArea)
 
-        # Draw the largest contour in the final result image
         result = np.zeros_like(image, dtype=np.uint8)
         cv.drawContours(result, [largest_contour], -1, 255, cv.FILLED)
 
@@ -105,7 +92,6 @@ class FeatureCalculationTransformer(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X):
-        # X is an array of processed images
         feature_names = ['area', 'perimeter', 'defects', 'extent']
         data = np.empty((0, len(feature_names)), float)
         target = []
@@ -115,9 +101,7 @@ class FeatureCalculationTransformer(BaseEstimator, TransformerMixin):
                 continue
 
             gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-
             contour = self.getLargestContour(gray)
-
             features = self.getContourFeatures(contour)
 
             data = np.append(data, np.array([features]), axis=0)
@@ -130,16 +114,6 @@ class FeatureCalculationTransformer(BaseEstimator, TransformerMixin):
 
         return dataset
 
-    def getContourExtremes(self, contour):
-        # determine the most extreme points along the contour
-        left = contour[contour[:, 0].argmin()]
-        right = contour[contour[:, 0].argmax()]
-        top = contour[contour[:, 1].argmin()]
-        bottom = contour[contour[:, 1].argmax()]
-
-        return np.array((left, right, top, bottom))
-
-
     def getConvexityDefects(self, contour):
         hull = cv.convexHull(contour, returnPoints=False)
         defects = cv.convexityDefects(contour, hull)
@@ -148,31 +122,28 @@ class FeatureCalculationTransformer(BaseEstimator, TransformerMixin):
 
         return defects
 
+    def getConvexHullAreas(contour):
+        hull = cv.convexHull(contour)
+
+        return cv.contourArea(hull)
 
     def getContourFeatures(self, contour):
         try:
-            area = cv.contourArea(contour)
-            perimeter = cv.arcLength(contour, True)
-            extremePoints = self.getContourExtremes(contour)
-
-            equi_diameter = np.sqrt(4 * area / np.pi)
             defects = self.getConvexityDefects(contour)
-
             defects = defects.squeeze()
             defects = defects[defects[:, -1] > 10000]
-            total = cv.sumElems(defects[:, -1])[0]
+            totalDefects = cv.sumElems(defects[:, -1])[0]
 
-            x, y, w, h = cv.boundingRect(contour)
-            rect_area = w * h
-            extent = float(area) / rect_area
+            perimeter = cv.arcLength(contour, True)
 
-            features = np.array((area, perimeter, total, extent))
+            hullArea = cv.contourArea(contour)
+
+            features = np.array((totalDefects, perimeter, hullArea))
 
             return features
         except Exception as e:
             print(f"An error occurred: {e}")
             return None
-
 
     def getLargestContour(self, img_BW):
         contours, hier = cv.findContours(img_BW.copy(), cv.RETR_EXTERNAL,
@@ -185,7 +156,8 @@ class PreprocessingPipeline:
     def process(self, X):
         pipeline = Pipeline([
             ('hand_extraction', HandExtractionTransformer()),
-            ('feature_calculation', FeatureCalculationTransformer())
+            ('feature_calculation', FeatureCalculationTransformer()),
+
         ])
 
         dataset = pipeline.fit_transform(X)
